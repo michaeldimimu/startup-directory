926341605170c9351959f402f6e81da9aa61b3e7
Michael Salam <michaeldimimu@gmail.com>
2025-08-20 12:46:08 +0100

    Complete startup details page, create number of views pill

--- next.config.ts
+++ next.config.ts
@@ -14,11 +14,6 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
-  devIndicators: {
-    appIsrStatus: true,
-    buildActivity: true,
-    buildActivityPosition: "bottom-right",
-  },
 };
 
 export default nextConfig;
